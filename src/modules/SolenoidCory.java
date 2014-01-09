95fb4c0c449c9f0740bbe17e25d64e56b48a7878
spinerc <spinerc@007-28.bxsci.78x445.nycboe.org>
2014-01-09 15:59:48 -0500

    Created Solenoid class

---
+++ src/modules/SolenoidCory.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modules;
+
+import edu.wpi.first.wpilibj.Joystick;
+import mainframe.AvianceThread;
+
+/**
+ *
+ * @author spinerc
+ */
+public class SolenoidCory extends AvianceThread {
+    
+    //creating joystick
+    Joystick jRight = new Joystick(1);
+    Joystick jLeft = new Joystick(2);
+    
+    
+}
