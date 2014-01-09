b3f4cab60c5b717e96d974a88676391d451bc48a
cecchil <cecchil@007-26.bxsci.78x445.nycboe.org>
2014-01-09 16:16:08 -0500

    LucasDrive better drive code with option for neumatics

---
+++ src/modules/LucasSolenoid.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modules;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ *
+ * @author cecchil
+ */
+public class LucasSolenoid {
+  Joystick buttonJoy = new Joystick(1);
+  DoubleSolenoid piston1 = new DoubleSolenoid(1,2);  
+  boolean isPushed;
+  
+    public void iteration(){
+    isPushed = buttonJoy.getRawButton(1);
+        if(isPushed){
+    while(true){
+         piston1.set(DoubleSolenoid.Value.kForward);
+         isPushed = buttonJoy.getRawButton(1);
+         if(isPushed){
+             break;
+         }
+        }
+   piston1.set(DoubleSolenoid.Value.kOff);
+        }
+}
+}
