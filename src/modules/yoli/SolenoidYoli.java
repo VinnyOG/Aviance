450614ec174e5d6408033c0f530778b233565104
meydany <meydany@007-24.bxsci.78x445.nycboe.org>
2014-01-09 17:07:38 -0500

    Rewrote deleted code and created package

---
+++ src/modules/yoli/SolenoidYoli.java
@@ -0,0 +1,28 @@
+package modules.yoli;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ *
+ * @author meydany
+ */
+public class SolenoidYoli {
+      
+    public Joystick rightjoy = new Joystick(1);
+    DoubleSolenoid piston = new DoubleSolenoid(1, 2);
+    
+    protected void iteration() {
+        
+        boolean pressed1 = rightjoy.getRawButton(1);
+
+        DoubleSolenoid.Value opened = piston.get();
+        
+        if(pressed1 && opened == DoubleSolenoid.Value.kReverse) {
+            piston.set(DoubleSolenoid.Value.kForward);
+        } else if(pressed1 && opened == DoubleSolenoid.Value.kForward) {
+            piston.set(DoubleSolenoid.Value.kReverse);
+        }
+        
+    }     
+}
