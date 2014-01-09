c62f45d43954d47bb13da3aaca457a9cfa4c50f0
meydany <meydany@007-24.bxsci.78x445.nycboe.org>
2014-01-09 16:21:52 -0500

    Corrected file name

---
+++ src/modules/SolenoidYoli.java
@@ -0,0 +1,30 @@
+package modules;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
+
+/**
+ *
+ * @author meydany
+ */
+public class SolenoidYoli {
+      
+    public Joystick rightjoy = new Joystick(1);
+    DoubleSolenoid piston = new DoubleSolenoid(1, 2);
+    
+    boolean pressed1 = rightjoy.getRawButton(1);
+    boolean pressed2 = rightjoy.getRawButton(2);
+     
+    protected void iteration() {
+        
+        if(pressed1) {
+            piston.set(DoubleSolenoid.Value.kForward);
+        }
+        
+        if(pressed2) {
+            piston.set(DoubleSolenoid.Value.kReverse);
+        }
+        
+    }
+      
+}
