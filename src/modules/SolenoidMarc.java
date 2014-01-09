4d433d0e2d81cc7996c95c33efa242f052d55ce2
zitellim <zitellim@007-27.bxsci.78x445.nycboe.org>
2014-01-09 15:59:50 -0500

    Created DoubleSolenoid Class

---
+++ src/modules/SolenoidMarc.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package modules;
+
+import edu.wpi.first.wpilibj.Joystick;
+import mainframe.AvianceThread;
+
+/**
+ *
+ * @author zitellim
+ */
+public class SolenoidMarc extends AvianceThread {
+    Joystick Right = new Joystick(1);
+    Joystick Left = new Joystick(2);
+    
+    DoubleSolenoid
+    
+}
